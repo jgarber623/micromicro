c54a8ecd23adc459f6cdccf99f0e1e340e6b0ce8
Jason Garber <jason@sixtwothree.org>
2019-08-21 21:23:11 -0400

    Add rel_urls URL normalization spec

---
+++ spec/lib/micro_micro/parsed_document_rel_urls_spec.rb
@@ -0,0 +1,40 @@
+describe MicroMicro::ParsedDocument, '#rel_urls' do
+  let(:relative_base) { 'https://relative.example.com' }
+  let(:absolute_base) { 'https://absolute.example.com' }
+
+  context 'when markup contains relative URLs' do
+    let(:markup) { '<!doctype html><html><head><link rel="webmention" href="/webmentions"></head></html>' }
+
+    let(:results) do
+      OpenStruct.new(:"#{relative_base}/webmentions" => OpenStruct.new(rels: ['webmention']))
+    end
+
+    it 'returns an OpenStruct' do
+      expect(described_class.new(markup, relative_base).rel_urls).to eq(results)
+    end
+  end
+
+  context 'when markup contains <base> element with relative URL' do
+    let(:markup) { '<!doctype html><html><head><base href="/foo/bar"><link rel="webmention" href="webmentions"></head></html>' }
+
+    let(:results) do
+      OpenStruct.new(:"#{relative_base}/foo/webmentions" => OpenStruct.new(rels: ['webmention']))
+    end
+
+    it 'returns an OpenStruct' do
+      expect(described_class.new(markup, relative_base).rel_urls).to eq(results)
+    end
+  end
+
+  context 'when markup contains <base> element with absolute URL' do
+    let(:markup) { %(<!doctype html><html><head><base href="#{absolute_base}"><link rel="webmention" href="webmentions"></head></html>) }
+
+    let(:results) do
+      OpenStruct.new(:"#{absolute_base}/webmentions" => OpenStruct.new(rels: ['webmention']))
+    end
+
+    it 'returns an OpenStruct' do
+      expect(described_class.new(markup, relative_base).rel_urls).to eq(results)
+    end
+  end
+end
