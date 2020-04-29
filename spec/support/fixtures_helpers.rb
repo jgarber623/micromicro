9b7370aeed873365245c498172b202c05cc805af
Jason Garber <jason@sixtwothree.org>
2020-04-29 15:06:15 -0400

    Clean up fixtures helper

--- spec/support/fixtures_helpers.rb
+++ spec/support/fixtures_helpers.rb
@@ -1,31 +1,27 @@
 module FixturesHelpers
   module MicroformatsTestSuite
-    class << self
-      def test_case_base_path
-        File.expand_path('fixtures/microformats_test_suite/tests', __dir__)
-      end
+    TEST_CASE_TYPES = %w[
+      microformats-v2/h-adr
+      microformats-v2/h-card
+      microformats-v2/h-entry
+      microformats-v2/h-event
+      microformats-v2/h-feed
+      microformats-v2/h-geo
+      microformats-v2/h-product
+      microformats-v2/h-recipe
+      microformats-v2/h-resume
+      microformats-v2/h-review
+      microformats-v2/h-review-aggregate
+      microformats-v2/mixed
+      microformats-v2/rel
+    ].freeze
 
-      def test_case_file_paths
-        test_case_types.map { |test_case_type| Dir[File.join(test_case_base_path, test_case_type, '*.json')] }.flatten
-      end
+    def self.test_case_base_path
+      File.expand_path('fixtures/microformats_test_suite/tests', __dir__)
+    end
 
-      def test_case_types
-        %w[
-          microformats-v2/h-adr
-          microformats-v2/h-card
-          microformats-v2/h-entry
-          microformats-v2/h-event
-          microformats-v2/h-feed
-          microformats-v2/h-geo
-          microformats-v2/h-product
-          microformats-v2/h-recipe
-          microformats-v2/h-resume
-          microformats-v2/h-review
-          microformats-v2/h-review-aggregate
-          microformats-v2/mixed
-          microformats-v2/rel
-        ]
-      end
+    def self.test_case_file_paths
+      TEST_CASE_TYPES.map { |test_case_type| Dir[File.join(test_case_base_path, test_case_type, '*.json')] }.flatten
     end
 
     class TestCase
