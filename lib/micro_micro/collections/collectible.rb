34c377d9fef333f0944307d044e1fd895b0c0ecb
Jason Garber <jason@sixtwothree.org>
2020-04-29 16:10:52 -0400

    Update documentation

--- lib/micro_micro/collections/collectible.rb
+++ lib/micro_micro/collections/collectible.rb
@@ -14,20 +14,20 @@ module MicroMicro
 
       # @param method [Symbol, String]
       # @param values [List<String>]
-      # @return [MicroMicro::Item, MicroMicro::Property, nil]
+      # @return [MicroMicro::Item, MicroMicro::Property, MicroMicro::Relation, nil]
       def find_by(method, *values)
         members.find { |member| values.include?(member.public_send(method)) }
       end
 
       # @param method [Symbol, String]
       # @param values [List<String>]
-      # @return [Array<MicroMicro::Item, MicroMicro::Property>]
+      # @return [Array<MicroMicro::Item, MicroMicro::Property, MicroMicro::Relation>]
       def find_all_by(method, *values)
         members.select { |member| values.include?(member.public_send(method)) }
       end
 
-      # @param member [MicroMicro::Item, MicroMicro::Property]
-      # @return [Array<MicroMicro::Item, MicroMicro::Property>]
+      # @param member [MicroMicro::Item, MicroMicro::Property, MicroMicro::Relation]
+      # @return [Array<MicroMicro::Item, MicroMicro::Property, MicroMicro::Relation>]
       # TODO: raise an ArgumentError unless member.is_a?(self.class)
       def push(member)
         members.push(member)
