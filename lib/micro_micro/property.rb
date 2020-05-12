d46780e65baa5d4005b569a412cacade8d761722
Jason Garber <jason@sixtwothree.org>
2020-05-12 15:00:17 -0400

    Set item value to existing parser value for e-* properties

--- lib/micro_micro/property.rb
+++ lib/micro_micro/property.rb
@@ -101,7 +101,6 @@ module MicroMicro
       return unless item_node?
 
       obj_by_prefix = case prefix
-                      when 'e' then item
                       when 'p' then item.properties.find { |property| property.name == 'name' }
                       when 'u' then item.properties.find { |property| property.name == 'url' }
                       end
