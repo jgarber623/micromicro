65486bce3cdcf52298c7f4d8ad00f9319d0ab24f
Jason Garber <jason@sixtwothree.org>
2020-11-06 22:20:03 -0500

    Add MicroMicro::Collections::PropertiesCollection#names and MicroMicro::Collections::PropertiesCollection#values methods and specs

--- lib/micro_micro/collections/properties_collection.rb
+++ lib/micro_micro/collections/properties_collection.rb
@@ -1,9 +1,19 @@
 module MicroMicro
   module Collections
     class PropertiesCollection < BaseCollection
+      # @return [Array<String>]
+      def names
+        @names ||= map(&:name).uniq.sort
+      end
+
       # @return [Hash{Symbol => Array<String, Hash>}]
       def to_h
         group_by(&:name).symbolize_keys.deep_transform_values(&:value)
+      end
+
+      # @return [Array<String, Hash>]
+      def values
+        @values ||= map(&:value).uniq
       end
     end
   end
---
+++ spec/lib/micro_micro/collections/properties_collection_names_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe MicroMicro::Collections::PropertiesCollection, '#names' do
+  subject(:names) { MicroMicro.parse(markup, base_url).items.first.properties.names }
+
+  let(:base_url) { 'http://example.com' }
+  let(:markup) { '<!doctype html><html><body><article class="h-entry"><h1 class="p-name">Hello, world!</h1></article></body></html>' }
+
+  it 'returns an Array' do
+    expect(names).to eq(['name'])
+  end
+end
