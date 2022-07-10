01aa6c36102a26b83bd5bf23aabd26b5b310b87f
Jason Garber <jason@sixtwothree.org>
2022-07-09 23:41:07 -0400

    Fix method ordering and remove commented code

--- lib/micro_micro/collections/relationships_collection.rb
+++ lib/micro_micro/collections/relationships_collection.rb
@@ -5,24 +5,18 @@ module MicroMicro
     class RelationshipsCollection < BaseCollection
       # @see https://microformats.org/wiki/microformats2-parsing#parse_a_hyperlink_element_for_rel_microformats
       #
-      # @return [Hash{Symbol => Hash{Symbol => Array, String}}]
-      def group_by_url
-        group_by(&:href).symbolize_keys.transform_values { |relationships| relationships.first.to_h.slice!(:href) }
+      # @return [Hash{Symbol => Array<String>}]
+      def group_by_rel
+        each_with_object(Hash.new { |hash, key| hash[key] = [] }) do |member, hash|
+          member.rels.each { |rel| hash[rel] << member.href }
+        end.symbolize_keys.transform_values(&:uniq)
       end
 
       # @see https://microformats.org/wiki/microformats2-parsing#parse_a_hyperlink_element_for_rel_microformats
       #
-      # @return [Hash{Symbol => Array<String>}]
-      def group_by_rel
-        # flat_map do |member|
-        #   member.rels.map { |rel| [rel, member.href] }
-        # end.group_by(&:shift)
-        #    .symbolize_keys
-        #    .transform_values(&:flatten)
-        #    .transform_values(&:uniq)
-        each_with_object(Hash.new { |hash, key| hash[key] = [] }) do |member, hash|
-          member.rels.each { |rel| hash[rel] << member.href }
-        end.symbolize_keys.transform_values(&:uniq)
+      # @return [Hash{Symbol => Hash{Symbol => Array, String}}]
+      def group_by_url
+        group_by(&:href).symbolize_keys.transform_values { |relationships| relationships.first.to_h.slice!(:href) }
       end
 
       # @return [Array<String>]
