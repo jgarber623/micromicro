cf8153febc2944c235f06d5e2769e6bad7830d92
Jason Garber <jason@sixtwothree.org>
2022-08-25 11:36:57 -0400

    Improve YARD docs

--- lib/micro_micro/collections/items_collection.rb
+++ lib/micro_micro/collections/items_collection.rb
@@ -3,11 +3,19 @@
 module MicroMicro
   module Collections
     class ItemsCollection < BaseCollection
+      # Return an Array of this collection's {MicroMicro::Item}s as Hashes.
+      #
+      # @see MicroMicro::Item#to_h
+      #
       # @return [Array<Hash{Symbol => Array<String, Hash>}>]
       def to_a
         map(&:to_h)
       end
 
+      # Retrieve a unique Array of this collection's {MicroMicro::Item}s' types.
+      #
+      # @see MicroMicro::Item#types
+      #
       # @return [Array<String>]
       def types
         @types ||= flat_map(&:types).uniq.sort
