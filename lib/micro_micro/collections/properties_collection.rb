f9dba9671627f08c47b35aa9d4a228465680dcf9
Jason Garber <jason@sixtwothree.org>
2022-08-25 13:59:15 -0400

    Improve YARD docs

--- lib/micro_micro/collections/properties_collection.rb
+++ lib/micro_micro/collections/properties_collection.rb
@@ -3,26 +3,49 @@
 module MicroMicro
   module Collections
     class PropertiesCollection < BaseCollection
+      # Retrieve an Array of this collection's unique {MicroMicro::Property}
+      # names.
+      #
+      # @see MicroMicro::Property#name
+      #
       # @return [Array<String>]
       def names
         @names ||= map(&:name).uniq.sort
       end
 
+      # A collection of plain text {MicroMicro::Property}s parsed from the node.
+      #
+      # @see MicroMicro::Property#plain_text_property?
+      #
       # @return [MicroMicro::Collections::PropertiesCollection]
       def plain_text_properties
         self.class.new(select(&:plain_text_property?))
       end
 
+      # Return a Hash of this collection's {MicroMicro::Property}s as Arrays.
+      #
+      # @see MicroMicro::Property#name
+      # @see MicroMicro::Property#value
+      #
       # @return [Hash{Symbol => Array<String, Hash>}]
       def to_h
         group_by(&:name).symbolize_keys.deep_transform_values(&:value)
       end
 
+      # A collection of url {MicroMicro::Property}s parsed from the node.
+      #
+      # @see MicroMicro::Property#url_property?
+      #
       # @return [MicroMicro::Collections::PropertiesCollection]
       def url_properties
         self.class.new(select(&:url_property?))
       end
 
+      # Return an Array of this collection's unique {MicroMicro::Property}
+      # values.
+      #
+      # @see MicroMicro::Property#value
+      #
       # @return [Array<String, Hash>]
       def values
         @values ||= map(&:value).uniq
--- lib/micro_micro/collections/relationships_collection.rb
+++ lib/micro_micro/collections/relationships_collection.rb
@@ -3,7 +3,10 @@
 module MicroMicro
   module Collections
     class RelationshipsCollection < BaseCollection
-      # @see https://microformats.org/wiki/microformats2-parsing#parse_a_hyperlink_element_for_rel_microformats
+      # Return a Hash of this collection's {MicroMicro::Relationship}s grouped
+      # by their +rel+ attribute value.
+      #
+      # @see https://microformats.org/wiki/microformats2-parsing#parse_a_hyperlink_element_for_rel_microformats Parse a hyperlink element for rel microformats
       #
       # @return [Hash{Symbol => Array<String>}]
       def group_by_rel
@@ -12,18 +15,31 @@ module MicroMicro
         end.symbolize_keys.transform_values(&:uniq)
       end
 
-      # @see https://microformats.org/wiki/microformats2-parsing#parse_a_hyperlink_element_for_rel_microformats
+      # Return a Hash of this collection's {MicroMicro::Relationship}s grouped
+      # by their +href+ attribute value.
+      #
+      # @see https://microformats.org/wiki/microformats2-parsing#parse_a_hyperlink_element_for_rel_microformats Parse a hyperlink element for rel microformats
       #
       # @return [Hash{Symbol => Hash{Symbol => Array, String}}]
       def group_by_url
         group_by(&:href).symbolize_keys.transform_values { |relationships| relationships.first.to_h.slice!(:href) }
       end
 
+      # Retrieve an Array of this collection's unique {MicroMicro::Relationship}
+      # +rel+ attrivute values.
+      #
+      # @see MicroMicro::Relationship#rels
+      #
       # @return [Array<String>]
       def rels
         @rels ||= flat_map(&:rels).uniq.sort
       end
 
+      # Retrieve an Array of this collection's unique {MicroMicro::Relationship}
+      # +href+ attribute values.
+      #
+      # @see MicroMicro::Relationship#urls
+      #
       # @return [Array<String>]
       def urls
         @urls ||= map(&:href).uniq.sort
