82e91c859a2cb8639cd5b19beae9c31f0db9dcc5
Jason Garber <jason@sixtwothree.org>
2022-08-26 12:26:18 -0400

    Add predicate methods to PropertiesCollection and update specs

--- lib/micro_micro/collections/properties_collection.rb
+++ lib/micro_micro/collections/properties_collection.rb
@@ -19,7 +19,15 @@ module MicroMicro
       #
       # @return [MicroMicro::Collections::PropertiesCollection]
       def plain_text_properties
-        self.class.new(select(&:plain_text_property?))
+        @plain_text_properties ||= self.class.new(select(&:plain_text_property?))
+      end
+
+      # Does this {MicroMicro::Collections::PropertiesCollection} include any
+      # plain text {MicroMicro::Property}s?
+      #
+      # @return [Boolean]
+      def plain_text_properties?
+        plain_text_properties.any?
       end
 
       # Return a Hash of this collection's {MicroMicro::Property}s as Arrays.
@@ -38,7 +46,15 @@ module MicroMicro
       #
       # @return [MicroMicro::Collections::PropertiesCollection]
       def url_properties
-        self.class.new(select(&:url_property?))
+        @url_properties ||= self.class.new(select(&:url_property?))
+      end
+
+      # Does this {MicroMicro::Collections::PropertiesCollection} include any
+      # url {MicroMicro::Property}s?
+      #
+      # @return [Boolean]
+      def url_properties?
+        url_properties.any?
       end
 
       # Return an Array of this collection's unique {MicroMicro::Property}
--- spec/lib/micro_micro/collections/properties_collection_spec.rb
+++ spec/lib/micro_micro/collections/properties_collection_spec.rb
@@ -8,13 +8,33 @@ RSpec.describe MicroMicro::Collections::PropertiesCollection do
   let(:markup) do
     <<~'HTML'.chomp
       <article class="h-entry">
-        <h1 class="p-name">Hello, world!</h1>
-        <a href="http://example.com" class="p-author h-card">Jason Garber</a>
+        <h1 class="p-name">
+          <a href="https://jgarber.example/posts/hello-world" class="u-url">Hello, world!</a>
+        </h1>
+        <a href="https://jgarber.example" class="p-author h-card">Jason Garber</a>
       </article>
     HTML
   end
 
-  its(:names) { is_expected.to eq(%w[author name]) }
+  its(:names) { is_expected.to eq(%w[author name url]) }
 
-  its(:values) { is_expected.to eq(['Hello, world!', { properties: { name: ['Jason Garber'], url: ['http://example.com/'] }, type: ['h-card'], value: 'Jason Garber' }]) }
+  its(:values) do
+    is_expected.to eq(
+      [
+        'Hello, world!',
+        'https://jgarber.example/posts/hello-world',
+        { properties: { name: ['Jason Garber'], url: ['https://jgarber.example/'] }, type: ['h-card'], value: 'Jason Garber' }
+      ]
+    )
+  end
+
+  its(:plain_text_properties) { is_expected.to be_a(described_class) }
+  its('plain_text_properties.first.name') { is_expected.to eq('name') }
+
+  its(:plain_text_properties?) { is_expected.to be(true) }
+
+  its(:url_properties) { is_expected.to be_a(described_class) }
+  its('url_properties.first.name') { is_expected.to eq('url') }
+
+  its(:url_properties?) { is_expected.to be(true) }
 end
